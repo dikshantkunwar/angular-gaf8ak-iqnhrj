d80860aa2dac8b68b6dc6330af8918e76aeb7dfa
dikshantkunwar <dikshantkunwar@gmail.com>
2022-03-04 08:21:21 -0500

    3 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -7,6 +8,7 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = [];
 
+  constructor(private http: HttpClient) {}
   addToCart(product: Product) {
     this.items.push(product);
   }
@@ -18,5 +20,11 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
